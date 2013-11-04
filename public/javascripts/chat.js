f146b92124870ea62d09e08c5ea85f8dda7beb21
Jennifer and Isaac <student@appacademy.io>
2013-11-04 15:18:45 -0800

    Add nicknames

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,14 +3,36 @@
 
 	var Chat = ChatClient.Chat = function (socket) {
 		this.socket = socket;
+
+		var that = this;
+		this.socket.on("nicknameChangeResult", function(data) {
+			if(data.success) {
+				that.nickname = data.name;
+			} else {
+				console.log("failed nickname change")
+			}
+		});
 	}
 
 	Chat.prototype.sendMessage = function (msg) {
-		this.socket.emit('message', msg)
+		if(msg.slice(0, 1) === "/") {
+			this.processCommand(msg);
+		} else {
+			var message = {
+				text: msg
+			}
+			this.socket.emit('message', message)
+		}
 	}
 
 	Chat.prototype.on = function(eventName, callback) {
 		this.socket.on(eventName, callback)
+	}
+
+	Chat.prototype.processCommand = function(msg) {
+		if(msg.slice(0, 5) === "/nick") {
+			this.socket.emit("nicknameChangeRequest", msg.slice(6));
+		}
 	}
 
 })(this);
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,22 +1,36 @@
 (function (root) {
 	var ChatClient = root.ChatClient = (root.ChatClient || {})
 
-	var socket = io.connect("http://localhost:8080");
+	var socket = io.connect("http://localhost:8080", { "sync disconnect on unload": true });
 	var chat = new ChatClient.Chat(socket);
 
 	$(document).ready( function () {
 		$('#message-form').on("submit", function (event) {
 			event.preventDefault();
+
 			var messageText = $('#message').val();
-			chat.sendMessage(messageText);
+			if(messageText.slice(0, 1) === "/") {
+				chat.processCommand(messageText);
+			} else {
+				chat.sendMessage(messageText);
+			}
+
+			$('#message').val("");
 		});
 	})
 
 	chat.on('message', function (data) {
 		console.log(data)
 		console.log(data.text);
-		$li = $("<li>").text(data.text)
+		$li = $("<li>").text(data.nickname + ": " + data.text)
 		$('ul.chat-content').append($li);
 	});
 
+	chat.on("roomListUpdate", function(data) {
+		var $el = $(".client-list");
+		$el.empty();
+		data.nicknameList.forEach(function(user) {
+			$el.append("<li>" + user + "</li>");
+		})
+	});
 })(this);
